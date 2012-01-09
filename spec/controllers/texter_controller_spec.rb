81ff170ffc332e2d8a911cf34175f0e03ce2f34c
Khem Veasna <kveasna@gmail.com>
2012-01-09 13:19:25 -0500

    Added more tests.

--- spec/controllers/texter_controller_spec.rb
+++ spec/controllers/texter_controller_spec.rb
@@ -1,62 +1,107 @@
 require 'spec_helper'
 
 describe TexterController do
-   
-  before(:each) do
-    # create an account for testing
-    @account = Factory(:account)
-   
-    sign_in @account
+  
+  def mock_twilio_client
+    twilio_client = double('twilio')
   end
   
-  describe 'validate provision' do
-    it "should login successfully" do
-      timestamp = Time.now.to_i
-      app_id = @account.application_id
-      secure_parameters = {timestamp: timestamp.to_s, application_id: app_id, anypresence_auth: Digest::SHA1.hexdigest("#{ENV['SHARED_SECRET']}-#{app_id}-#{timestamp}") }
-    
-      post :provision, :application_id => secure_parameters[:application_id], :anypresence_auth => secure_parameters[:anypresence_auth], :timestamp => secure_parameters[:timestamp]
-      Rails.logger.info "param: " + @secure_parameters.inspect
-      Rails.logger.info "login: " + response.body.inspect
+  def mock_twilio_account
+    twilio_client = double('account')
+  end
+  
+  describe "verify login" do
+    before(:each) do
+      # create an account for testing
+      @account = Factory.build(:account)
+      sign_in @account
     end
     
-    it "should login unsuccessfully with different application_id" do
-      timestamp = Time.now.to_i
-      app_id = @account.application_id.to_i + 1;
-      app_id = app_id.to_s
-      secure_parameters = {timestamp: timestamp.to_s, application_id: app_id, anypresence_auth: Digest::SHA1.hexdigest("#{ENV['SHARED_SECRET']}-#{app_id}-#{timestamp}") }
-    
-      post :provision, :application_id => secure_parameters[:application_id], :anypresence_auth => secure_parameters[:anypresence_auth], :timestamp => secure_parameters[:timestamp]
-      Rails.logger.info "param: " + @secure_parameters.inspect
-      Rails.logger.info "login: " + response.body.inspect
-    end    
+    describe 'validate provision' do
+      it "should login successfully" do
+        timestamp = Time.now.to_i
+        app_id = @account.application_id
+        secure_parameters = {timestamp: timestamp.to_s, application_id: app_id, anypresence_auth: Digest::SHA1.hexdigest("#{ENV['SHARED_SECRET']}-#{app_id}-#{timestamp}") }
+      
+        post :provision, :application_id => secure_parameters[:application_id], :anypresence_auth => secure_parameters[:anypresence_auth], :timestamp => secure_parameters[:timestamp]
+        parsed_body = JSON.parse(response.body)
+        parsed_body["success"].should == true    
+      end
+      
+      it "should login unsuccessfully with different application_id" do
+        timestamp = Time.now.to_i
+        app_id = @account.application_id.to_i + 1;
+        app_id = app_id.to_s
+        secure_parameters = {timestamp: timestamp.to_s, application_id: app_id, anypresence_auth: Digest::SHA1.hexdigest("#{ENV['SHARED_SECRET']}-#{app_id}-#{timestamp}") }
+      
+        post :provision, :application_id => secure_parameters[:application_id], :anypresence_auth => secure_parameters[:anypresence_auth], :timestamp => secure_parameters[:timestamp]
+        response.body.should 
+      end    
+    end
   end
     
   describe 'perform text' do
-    #it "should text sucessfully with phone number" do
-    #  @account.phone_number = '+19789445741'
+    before(:each) do
+      # create an account for testing
+      @account = Factory.create(:account)
+      sign_in @account
+    end
+
+    it "should text sucessfully with phone number" do
+      twilio_client = mock_twilio_client
+      twilio_account = mock_twilio_account
+      subject.current_account.phone_number = "+16178613962" # a state rejection number
+      Twilio::REST::Client.should_receive(:new).with(any_args()).and_return(twilio_client)
+
+      twilio_client.stub(:account).and_return(twilio_account)
+      sms = double('sms')
+      twilio_account.stub_chain(:sms,:messages,:create).and_return(sms)
       
-    #  post :text
-    #  parsed_body = JSON.parse(response.body)
-    #  parsed_body["success"].should == true
-    #end
+      post :text
+      
+      parsed_body = JSON.parse(response.body)
+      parsed_body["success"].should == true
+    end
+
+    it "should text unsuccessfully with json error message when exception is thrown" do
+      twilio_client = mock_twilio_client
+      twilio_account = mock_twilio_account
+      subject.current_account.phone_number = "+16178613962" # a state rejection number
+      Twilio::REST::Client.should_receive(:new).with(any_args()).and_return(twilio_client)
+
+      twilio_client.stub(:account).and_return(twilio_account)
+      
+      sms = double('sms')
+      twilio_account.stub_chain(:sms,:messages,:create).and_raise("Error when trying to text.")
+      
+      post :text
+      
+      parsed_body = JSON.parse(response.body)
+      parsed_body["success"].should == false      
+    end
     
     it "should text unsucessfully without phone number" do
       subject.current_account.phone_number = nil;
-
       post :text
       response.body.should == "Not yet set up!"
     end
-    
+         
   end
-    
-  describe "Get consume" do
+  
+  describe 'perform sms consumption' do
+ 
+    before(:each) do
+      # create an account for testing
+      @account = Factory.create(:account)
+      sign_in @account
+    end     
+     
     it "should consume without error" do
-      post :consume, :smsMessageSid => '1234', :accountSid => '1234', :body => 'test message', :from => '4155992671', :to => '9789445741'
-      messages = Message.all
+      post :consume, :sms_message_sid => '1234', :account_sid => '1234', :body => 'test message', :from => '4155992671', :to => '9789445741'
+   
       parsed_body = JSON.parse(response.body)
       parsed_body["success"].should == true
     end
   end
-  
+
 end
