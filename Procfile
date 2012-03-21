3a210ace5c4f89d9e99dc27593cfedf26f40102b
Khem Veasna <kveasna@anypresence.com>
2012-03-21 12:02:21 -0400

    Added resque logging.

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec rails server mongrel -p $PORT
+web: bundle exec rails server -p $PORT
 worker: bundle exec rake resque:work QUEUE=*
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,5 @@
 Resque.redis = REDIS
+
+Resque.after_fork = Proc.new do
+  Rails.logger.auto_flushing = true if Rails.logger.respond_to?(:auto_flushing)
+end
