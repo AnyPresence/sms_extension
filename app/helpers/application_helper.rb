7f9bcd72580a9fb4f3507eb3ed5f7fac4543b8b4
Khem Veasna <kveasna@anypresence.com>
2012-03-26 12:49:59 -0400

    Tweaked UI.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def chameleon_stylesheet_link
-    uri = CSS_URIS.last
+    uri = "/assets/application.css"
     uri = "#{ENV['CHAMELEON_HOST'].strip.gsub(/\/+$/, '')}#{uri}"
     stylesheet_link_tag(uri)
   end
