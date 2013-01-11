924c11cbf9e1e59996a2818d14b83c443bd1d5e4
Khem Veasna <kveasna@anypresence.com>
2013-01-11 14:53:04 -0500

    Updated Gemfile to work with test dummy app; ensure that only admins have access.

--- app/controllers/sms_extension/application_controller.rb
+++ app/controllers/sms_extension/application_controller.rb
@@ -1,5 +1,6 @@
 module SmsExtension
   class ApplicationController < ActionController::Base
+    before_filter :authenticate_admin!
   end
 end
 
--- spec/dummy/config/initializers/extensions.rb
+++ spec/dummy/config/initializers/extensions.rb
@@ -1,1 +1,6 @@
 AP::SmsExtension::Sms::config_account({:phone_number => "1234", :outgoing_message_format => "howdy {{title}}"})
+
+class ActionController::Base
+  def authenticate_admin!
+  end
+end
